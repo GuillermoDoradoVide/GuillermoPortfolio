31fa740790678f0e297f9c88182e985144b9d483
Guillermo Dorado Vide <guillermo.dorado@telefonica.net>
2016-09-29 19:59:22 +0200

    Añadido efecto a la hora de mostrar la información principal de los proyectos. Se ha empezado el sistema para mostrar y ocultar el resto de proyectos.

--- js/nav-js-main.js
+++ js/nav-js-main.js
@@ -1,13 +1,44 @@
 /*javascript*/
-var sub_menu = document.getElementsByTagName("article");
+var sub_menu = document.getElementsByTagName("section");
+function init() {}
+window.onload = init;
 
 function expandirInfo(num_proyecto) {
-	for(var x = 0; x < sub_menu.length; x++) {
-		if(sub_menu[x] == num_proyecto) {
-			console.log("miau");
+	var container;
+		container = num_proyecto.getElementsByClassName("container-cell")[0];
+		if (container.classList.contains("container-cell-hiding")) {
+			container.classList.remove("container-cell-hiding");
+			container.classList.add("container-cell-showing");
+			hideOtherProyects(num_proyecto);
 		}
 		else {
+			container.classList.remove("container-cell-showing");
+			container.classList.add("container-cell-hiding");
+			//unhideOtherProyects(num_proyecto);
+		}
+}
 
+function hideOtherProyects(num_proyecto) {
+	var container;
+	for(var x = 0; x < sub_menu.length; x++) {
+		console.log("veces");
+		if( sub_menu[x] !== num_proyecto) {
+			console.log("veces if");
+			container = num_proyecto.parentNode;
+			console.log(container);
+			container.classList.add("container-main-hiding");
+			container.classList.remove("container-main-showing");
+		}
+	}
+}
+
+function unhideOtherProyects(num_proyecto) {
+	var container;
+	for(var x = 0; x < sub_menu.length; x++) {
+		if( sub_menu[x] !== num_proyecto) {
+			container = num_proyecto.parentNode;
+			container.classList.remove("container-main-hiding");
+			container.classList.add("container-main-showing");
 		}
 	}
 }
