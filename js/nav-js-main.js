15502d53761799464ab48a1e9eb3748d58f963d6
Guillermo Dorado Vide <guillermo.dorado@telefonica.net>
2016-09-29 18:14:59 +0200

    Subido un js de prueba.

--- js/nav-js-main.js
+++ js/nav-js-main.js
@@ -1,3 +1,13 @@
 /*javascript*/
-var sub_menu;
-sub_menu=
+var sub_menu = document.getEletmentsByTagName("article");
+
+function expandirInfo(num_proyecto) {
+	for(var x = 0; x < sub_menu.length; x++) {
+		if(sub_menu[x] == num_proyecto) {
+			console.log("miau");
+		}
+		else {
+
+		}
+	}
+}
