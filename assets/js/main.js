257e0421d8189a263b13cc002c5540c1b65d462c
Guillermo Dorado Vide <guillermo.dorado@telefonica.net>
2016-10-03 13:39:41 +0200

    Modificado el estilo de la página de proyecto. [Por hacer]: Enlaces, contacto, información.

--- assets/js/main.js
+++ assets/js/main.js
@@ -209,6 +209,34 @@
 						}
 					});
 
+			// Find all YouTube videos
+			var $allVideos = $("iframe[src^='//www.youtube.com']"),
+			    // The element that is fluid width
+		    $fluidEl = $("body");
+			// Figure out and save aspect ratio for each video
+			$allVideos.each(function() {
+			  $(this)
+			    .data('aspectRatio', this.height / this.width)
+			    // and remove the hard coded width/height
+			    .removeAttr('height')
+			    .removeAttr('width');
+				console.log("a ver");
+			});
+			// When the window is resized
+			$(window).resize(function() {
+				console.log("hola");
+		  		var newWidth = $fluidEl.width();
+			 	// Resize all videos according to their own aspect ratio
+			 	$allVideos.each(function() {
+			    	var $el = $(this);
+			    	$el
+			      		.width(newWidth)
+			      		.height(newWidth * $el.data('aspectRatio'));
+
+			  	});
+			// Kick off one resize to fix all videos on page load
+			}).resize();
+
 	});
 
 })(jQuery);
